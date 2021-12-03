2b424fcc685ce3b0c5c8b8367cdb51c7d80c49e9
Betty <betty_elfagid@yahoo.co.uk>
2021-12-03 02:10:05 +0000

    provider added in to index.js file and store passed as a prop in to our provider

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import booksReducer from './reducers/booksReducer';
+import booksReducer from '../reducers/booksReducer';
 
 export default createStore(booksReducer, applyMiddleware(thunk))

946dabf6d05058b189b7fdf79b99e64bde15803d
Betty <betty_elfagid@yahoo.co.uk>
2021-12-07 00:12:17 +0000

    simple styling added for bookform

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class BookForm extends React.Component {
     render(){
         return(
-            <div>
-                <form>
-                     <lable htmlFor="image-input">Image</lable>
-                      <input id="image-input" type="text" />
-                     <lable htmlFor="title-input">Title</lable>
-                      <input id="title-input" type="text" />
-                     <lable htmlFor="author-input">Author</lable>
-                      <input id="author-input" type="text" />
-                     <lable htmlFor="description-input">Description</lable>
-                      <input id="description-input" type="text" />
+            <div className="book-container" >
+
+                 <div>
+                   <NavLink to='/'> Home </NavLink>
+                </div>
+                <br/>
+                <h3> Add Book </h3>
+                <form className="book-form">
+                   
+                     <lable htmlFor="title-input">Title:</lable>
+                      <input id="title-input" type="text" /><br/>
+                     <lable htmlFor="author-input">Author:</lable>
+                      <input id="author-input" type="text" /><br/>
+                     <lable htmlFor="description-input">Description:</lable>
+                      <input id="description-input" type="text" /><br/>
+                      <lable htmlFor="image-input">Upload Image:</lable>
+                      <input id="image-input" type="file" /> <br/>
                      <button type="submit">Submit</button>
                 </form>
 
-                <div>
-                 <Link to='/'> Home </Link>
-                </div>
+                
             </div>
         )
     }

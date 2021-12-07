f7bbf9d4d8f8d3fbe9dcd9ca288ab221df5e9816
Betty <betty_elfagid@yahoo.co.uk>
2021-12-07 00:43:43 +0000

    state and input value added to bookform

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class BookForm extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           title:"",
+           author:"",
+           description:"",
+           image:""
+       }
+   }
+
     render(){
         return(
             <div className="book-container" >
-
                  <div>
                    <NavLink to='/'> Home </NavLink>
                 </div>
@@ -14,11 +23,11 @@ export default class BookForm extends React.Component {
                 <form className="book-form">
                    
                      <lable htmlFor="title-input">Title:</lable>
-                      <input id="title-input" type="text" /><br/>
+                      <input id="title-input" type="text" value={this.state.title} /><br/>
                      <lable htmlFor="author-input">Author:</lable>
-                      <input id="author-input" type="text" /><br/>
+                      <input id="author-input" type="text" value={this.state.author}/><br/>
                      <lable htmlFor="description-input">Description:</lable>
-                      <input id="description-input" type="text" /><br/>
+                      <input id="description-input" type="textarea" value={this.state.description}/><br/>
                       <lable htmlFor="image-input">Upload Image:</lable>
                       <input id="image-input" type="file" /> <br/>
                      <button type="submit">Submit</button>

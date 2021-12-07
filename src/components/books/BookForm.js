23c7b27d77d5f45da6c7f4c9e2de409358d29073
Betty <betty_elfagid@yahoo.co.uk>
2021-12-07 01:04:59 +0000

    handleChange function added to bookform

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class BookForm extends React.Component {
-   constructor(props) {
-       super(props)
-       this.state = {
+
+  state = {
            title:"",
            author:"",
            description:"",
            image:""
        }
-   }
+
+       handleChange = (e) => {
+        this.setState({
+           [e.target.name] : e.target.value
+        })
+      }
 
     render(){
         return(
@@ -23,14 +27,15 @@ export default class BookForm extends React.Component {
                 <form className="book-form">
                    
                      <lable htmlFor="title-input">Title:</lable>
-                      <input id="title-input" type="text" value={this.state.title} /><br/>
+                      <input name="title" id="title-input" type="text" value={this.state.title} onChange={this.handleChange} /><br/>
                      <lable htmlFor="author-input">Author:</lable>
-                      <input id="author-input" type="text" value={this.state.author}/><br/>
+                      <input name="author" id="author-input" type="text" value={this.state.author} onChange={this.handleChange} /><br/>
                      <lable htmlFor="description-input">Description:</lable>
-                      <input id="description-input" type="textarea" value={this.state.description}/><br/>
+                      <input name="description" id="description-input" type="textarea" value={this.state.description} onChange={this.handleChange} /><br/>
                       <lable htmlFor="image-input">Upload Image:</lable>
-                      <input id="image-input" type="file" /> <br/>
+                      <input name="image" id="image-input" type="file" onChange={this.handleChang} /> <br/>
                      <button type="submit">Submit</button>
+                     {this.state.title}
                 </form>
 
                 
